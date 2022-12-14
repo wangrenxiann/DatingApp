480da1b41cb41f38bd560e86e58bd79a8f23ac22
wangrenxiann <2003931I@student.tp.edu.sg>
2022-12-14 20:59:18 +0800

    updated login and navmenu colour

--- DatingApp/Server/Models/ApplicationUser.cs
+++ DatingApp/Server/Models/ApplicationUser.cs
@@ -8,5 +8,7 @@ namespace DatingApp.Server.Models
 {
     public class ApplicationUser : IdentityUser
     {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
     }
 }
