d31be0e8deddd5262c104251caaaaca69ed30004
wangrenxiann <2003931I@student.tp.edu.sg>
2023-02-07 00:52:14 +0800

    player messages updated CRUD

--- DatingApp/Shared/Domain/Message.cs
+++ DatingApp/Shared/Domain/Message.cs
@@ -3,11 +3,14 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
 
 namespace DatingApp.Shared.Domain
 {
     public class Message : BaseDomainModel
     {
+        [Required]
+        [StringLength(100, MinimumLength = 2, ErrorMessage = "Wah lao so short meh")]
         public string MessageDesc { get; set; }
     }
 }
